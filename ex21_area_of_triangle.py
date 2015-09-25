78fb0e8f1d64f9a402ffb523f7feadd5c0f4d010
Ivan Vucetic <vuceta87@yahoo.com>
2015-09-25 18:31:13 +0200

    exercise 21

---
+++ ex21_area_of_triangle.py
@@ -0,0 +1,21 @@
+# Exercises from the book:
+# Ben Stephenson - The Python Workbook
+#
+# Exercise no. 21
+# Date: 25.09.2015
+#
+# Ivan Vucetic
+# vuceta87@yahoo.com
+
+
+# Exercise 21: Area of a Triangle
+
+print "Enter the base of the triangle:"
+b = float(raw_input("> "))
+
+print "Enter the height of the triangle:"
+h = float(raw_input("> "))
+
+area = b * h / 2
+
+print "Area of the triangle is", round(area, 2)
