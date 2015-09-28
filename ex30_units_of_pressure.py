c0e9855bc2a54e54a4c642d3be7e6f9e6144cfdc
Ivan Vucetic <vuceta87@yahoo.com>
2015-09-28 10:25:06 +0200

    exercise 30 initial

---
+++ ex30_units_of_pressure.py
@@ -0,0 +1,11 @@
+# Exercises from the book:
+# Ben Stephenson - The Python Workbook
+#
+# Exercise no. 30
+# Date: 28.09.2015
+#
+# Ivan Vucetic
+# vuceta87@yahoo.com
+
+
+# Exercise 30: Units of Pressure
