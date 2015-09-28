38cb633540bde3a1fc88a8d0cf5e8af2275fd9f0
Ivan Vucetic <vuceta87@yahoo.com>
2015-09-28 16:01:28 +0200

    exercise 34

---
+++ ex34_even_or_odd.py
@@ -0,0 +1,19 @@
+# Exercises from the book:
+# Ben Stephenson - The Python Workbook
+#
+# Exercise no. 34
+# Date: 28.09.2015
+#
+# Ivan Vucetic
+# vuceta87@yahoo.com
+
+
+# Exercise 34: Even or Odd?
+
+print "Insert an integer:"
+num = int(raw_input("> "))
+
+if num % 2 == 0:
+	print "EVEN"
+else:
+	print "ODD"
