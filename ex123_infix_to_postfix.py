564f0901b93c624fd4d7f466de5c7d93b5b21230
Ivan Vucetic <vuceta87@yahoo.com>
2015-10-12 14:37:26 +0200

    exercise 123

---
+++ ex123_infix_to_postfix.py
@@ -0,0 +1,43 @@
+# Exercises from the book:
+# Ben Stephenson - The Python Workbook
+#
+# Exercise no. 123
+# Date: 07.10.2015
+#
+# Ivan Vucetic
+# vuceta87@yahoo.com
+
+
+# Exercise 123: Infix to Postfix
+
+def infix2postfix(infix):
+	from ex90_does_a_string_represent_an_integer import is_integer
+	from ex91_operator_precedence import precedence
+
+	operators = []
+	postfix = []
+
+	for token in infix:
+		if token.isdigit(): # ! ex.90---> program bad, recognizes + and - as int
+			postfix.append(token)
+		if token in "*/+-^": 
+			while (len(operators) > 0) and (operators[-1] != '(') and \
+				(precedence(token) < precedence(operators[-1])): # ex.91
+				postfix.append(operators.pop())
+			operators.append(token)
+		if token == "(":
+			operators.append(token)
+		if token == ")":
+			while operators[-1] != "(":
+				postfix.append(operators.pop())
+			operators = operators[:-1]
+
+	while len(operators) > 0:
+		postfix.append(operators.pop())
+
+	return postfix
+
+
+li = ['(','1','+', '3',')', '+', '(','4', '+', '3',')']
+print infix2postfix(li)
+
