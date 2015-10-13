e7d98929a7dca094c64cf3264ee723a72529f3c5
Ivan Vucetic <vuceta87@yahoo.com>
2015-10-13 17:18:07 +0200

    exercise 134

---
+++ ex134_unique_characters.py
@@ -0,0 +1,23 @@
+# Exercises from the book:
+# Ben Stephenson - The Python Workbook
+#
+# Exercise no. 134
+# Date: 13.10.2015
+#
+# Ivan Vucetic
+# vuceta87@yahoo.com
+
+
+# Exercise 134: Unique Characters
+
+from sets import Set
+string = raw_input("Insert text: ")
+unique = Set([])
+
+for char in string:
+	if char in unique:
+		pass
+	else:
+		unique.add(char)
+
+print "Number of unique characters:", len(unique)
